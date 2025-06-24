601304951a1322e94168a13120e37d4e315ecb94
saadaltafofficial <saadbeenco@gmail.com>
2025-06-24 20:23:05 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from "./db.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id == args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((a) => a.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((g) => g.id === parent.game_id);
+    },
+  },
+  Mutation: {
+        deleteGame(_, args) {
+            game = db.games.filter(games => games.id !== args.id)
+            return game
+        },
+        addGame(_, args) {
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random() * 10000).toString()
+            }
+            db.games.push(game)
+            return game
+        },
+        updateGame(_, args) {
+            db.games = db.games.map(g => {
+                if (g.id === args.id) {
+                    return {...g, ...args.edits}
+                }
+                return g
+            })
+
+            return db.games.find(g => g.id == args.id)
+        }
+    }
+};
+
+const server = new ApolloServer({
+  // typeDefination is the schema that defines the structure of the data and how to query it
+  // resolver is the function that resolves the queries
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
